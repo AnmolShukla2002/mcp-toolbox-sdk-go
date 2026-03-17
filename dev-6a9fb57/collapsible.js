8bc99d0cd8c973a640c663808ed467c86d581286
AnmolShukla2002 <AnmolShukla2002@users.noreply.github.com>
2026-03-17 10:42:19 +0000

    deploy: 6a9fb5700aaaecd466e0c86723d5f05f576b77c9

---
+++ dev-6a9fb57/collapsible.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const directorySection = document.querySelector('.UnitDirectories');
+  if (!directorySection) return;
+  
+  const rows = Array.from(directorySection.querySelectorAll('tr'));
+});
